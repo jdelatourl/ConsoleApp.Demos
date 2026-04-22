c3e10588a80bd5bd302fb88fa95292b312b31be9
Jean Delatour <jdelatourl@yahoo.com>
2026-04-22 15:40:00 -0400

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World! My name is Jean Delatour");
+
+Console.WriteLine("I am from Haiti!");
+
+
+Console.WriteLine("Anything you want to say");
+
+Console.Write("Line1");
+Console.Write("Line2");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+// Different datatypes
+/*
+    text - string
+    intergers - int
+    decimal - double, float, decimal
+    logical - bool
+ */
+
+string name = "Lenny Delatour";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name);  // String concatenation
+Console.WriteLine($"They call me {name}");  // String interpolation
+Console.WriteLine("I was given the name {0}", name);  // Formatted string
+
+int age = 97;
+int retirementYearsLeft = 15;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? " +  isRetired);
